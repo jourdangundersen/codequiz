db78bce7032456e92dee4210d58064032f68c614
Jourdan Gundersen <jourdangundersen@Jourdans-MacBook-Pro.local>
2021-01-05 19:29:59 -0600

    added a ton of semicolons

--- assets/script.js
+++ assets/script.js
@@ -36,7 +36,7 @@ var allquestions = [
         Option4: "!",
         Answer: "2"
     },
-]
+];
 var startbutton =document.getElementById("start");
 var firstcontainer = document.getElementById("firstcontainer");
 var secondcontainer = document.getElementById("secondcontainer");
@@ -50,72 +50,72 @@ var userinitials = document.getElementById("userinitials");
 var saveuser = document.getElementById("saveuser");
 var userscore = document.getElementById("userscore");
 var displayinitials = document.getElementById("displayinitials");
-var currentquestion = 0
-var rightanswer = 0
-var wronganswer = 0
+var currentquestion = 0;
+var rightanswer = 0;
+var wronganswer = 0;
 
-firstcontainer.style.display="none"
-secondcontainer.style.display="none"
+firstcontainer.style.display="none";
+secondcontainer.style.display="none";
 
 startbutton.addEventListener("click", function() {
-startbutton.style.display="none"
-firstcontainer.style.display="block"
-displayquestion()
-})
+startbutton.style.display="none";
+firstcontainer.style.display="block";
+displayquestion();
+});
 saveuser.addEventListener("click", function() {
-    var user = userinitials.value
-    var history = JSON.parse(localStorage.getItem("initials")) || []
+    var user = userinitials.value;
+    var history = JSON.parse(localStorage.getItem("initials")) || [];
     history.push({
         user:user, 
         score:rightanswer
-    })
-    var htmldata = ""
+    });
+    var htmldata = "";
     for (let i=0; i < history.length; i++){
-        htmldata += `<h5>User : ${history[i].user}  Score:${history[i].score}</h5>`
+        htmldata += `<h5>User : ${history[i].user}  Score:${history[i].score}</h5>`;
     }
-    displayinitials.innerHTML=htmldata
-    localStorage.setItem("initials",JSON.stringify(history))
-})
-option1.addEventListener("click", checkuseranswer)
-option2.addEventListener("click", checkuseranswer)
-option3.addEventListener("click", checkuseranswer)
-option4.addEventListener("click", checkuseranswer)
+    displayinitials.innerHTML=htmldata;
+    localStorage.setItem("initials",JSON.stringify(history));
+});
+option1.addEventListener("click", checkuseranswer);
+option2.addEventListener("click", checkuseranswer);
+option3.addEventListener("click", checkuseranswer);
+option4.addEventListener("click", checkuseranswer);
 function displayquestion(){
-question.innerText = allquestions[currentquestion].Question
-option1.innerText = allquestions[currentquestion].Option1
-option2.innerText = allquestions[currentquestion].Option2
-option3.innerText = allquestions[currentquestion].Option3
-option4.innerText = allquestions[currentquestion].Option4
+question.innerText = allquestions[currentquestion].Question;
+option1.innerText = allquestions[currentquestion].Option1;
+option2.innerText = allquestions[currentquestion].Option2;
+option3.innerText = allquestions[currentquestion].Option3;
+option4.innerText = allquestions[currentquestion].Option4;
 
 }
 function checkuseranswer(){
-    var userchoice = this.getAttribute("data-value")
+    var userchoice = this.getAttribute("data-value");
     if (userchoice == allquestions[currentquestion].Answer){
-        rightanswer ++ 
-        answer.textContent="Correct !!!"
+        rightanswer ++ ;
+        answer.textContent="Correct !!!";
     }
     else {
-        wronganswer ++
-        answer.innerText="incorrect"
+        wronganswer ++;
+        answer.innerText="incorrect";
     }
-    console.log(userchoice)
+    console.log(userchoice);
     if (currentquestion < allquestions.length-1) {
-        currentquestion ++ 
-        displayquestion()
+        currentquestion ++;
+        displayquestion();
     }
     else {
-        console.log(rightanswer, wronganswer)
-        displayuserscore()
+        console.log(rightanswer, wronganswer);
+        displayuserscore();
     }
 }
 function displayuserscore(){
-    firstcontainer.style.display="none"
-    secondcontainer.style.display="block"
-    userscore.innerText="rightanswer:" + rightanswer + "wronganswer" + wronganswer
-    var history = JSON.parse(localStorage.getItem("initials")) || []
-    var htmldata = ""
+    firstcontainer.style.display="none";
+    secondcontainer.style.display="block";
+    userscore.innerText="rightanswer:" + rightanswer + "wronganswer" + wronganswer;
+    var history = JSON.parse(localStorage.getItem("initials")) || [];
+    var htmldata = "";
     for (let i=0; i < history.length; i++){
-        htmldata += `<h5>User : ${history[i].user}  Score:${history[i].score}</h5>`
+        htmldata += `<h5>User : ${history[i].user}  Score:${history[i].score}</h5>`;
     }
-    displayinitials.innerHTML=htmldata
+    displayinitials.innerHTML=htmldata;
 }
